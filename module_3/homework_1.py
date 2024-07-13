ccd0157a99ef8fa559f055f71bce2e175b4fcb1a
IgorTP <coder.samurai.8020@gmail.com>
2024-07-13 18:41:21 +0300

    1 Update (3 module)

---
+++ module_3/homework_1.py
@@ -0,0 +1,13 @@
+import telebot
+from config import token
+
+bot = telebot.TeleBot(token=token)
+
+
+@bot.message_handler(content_types=["text"])
+def func(message: telebot.types.Message):
+    output_data = message.text * 10
+    bot.send_message(message.from_user.id, output_data)
+
+
+bot.polling(non_stop=True)
